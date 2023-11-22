014fccd1e3c0cfc1060ccf62d11ae36e2daf40ae
Prashant Prasad <83994388+Prashant2005@users.noreply.github.com>
2023-11-22 18:52:16 +0530

    Update file2.py

--- day1/file2.py
+++ day1/file2.py
@@ -1,9 +1,11 @@
 #Using selenium
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import pandas as pd
 driver=webdriver.Chrome()
+url=input("Enter url: ")
 driver.get(url)
 get_res=driver.find_element(By.TAG_NAME, 'body')
 
