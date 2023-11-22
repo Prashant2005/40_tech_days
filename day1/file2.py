fc6b2dd50b68ba292d9ebd20253c1d5fdeabe108
Prashant Prasad <83994388+Prashant2005@users.noreply.github.com>
2023-11-22 18:51:08 +0530

    Create file2.py

---
+++ day1/file2.py
@@ -0,0 +1,10 @@
+#Using selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pandas as pd
+driver=webdriver.Chrome()
+driver.get(url)
+get_res=driver.find_element(By.TAG_NAME, 'body')
+
+print(get_res.text)
