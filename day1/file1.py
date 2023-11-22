4a656627e417b840c54eb67d04b06387186c5dae
Prashant Prasad <83994388+Prashant2005@users.noreply.github.com>
2023-11-22 18:52:37 +0530

    Update file1.py

--- day1/file1.py
+++ day1/file1.py
@@ -1,10 +1,10 @@
 #simple program to scrape content using beautifulsoup and requests
 
-url=input("Enter url: ")
+
 
 import requests
 from bs4 import BeautifulSoup
-
+url=input("Enter url: ")
 r=requests.get(url)
 contents=r.content
 soup=BeautifulSoup(contents, 'html.parser')
